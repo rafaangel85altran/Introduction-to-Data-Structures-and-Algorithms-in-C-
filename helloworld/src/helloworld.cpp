bbfa01cef6f3671063e3cb8771d0cceaf56263c4
jrangel_segula <jrangel@segula.es>
2019-05-08 10:51:02 +0200

    Finally I´ve managed to set eclipse

---
+++ helloworld/src/helloworld.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : helloworld.cpp
+// Author      : jrangel
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello Wohld!!!" << endl; // prints !!!Hello World!!!
+	//test
+	return 0;
+}
